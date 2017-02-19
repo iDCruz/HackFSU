722bb7b6c54bc63082a5654f3098110b5946500d
jr7square <recinos@mail.usf.edu>
2017-02-19 08:34:02 -0500

    More Stuff

--- app/src/main/java/com/example/davidcruz/hackfsu/MyInterestsFragment.java
+++ app/src/main/java/com/example/davidcruz/hackfsu/MyInterestsFragment.java
@@ -3,8 +3,14 @@ package com.example.davidcruz.hackfsu;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
@@ -19,6 +25,11 @@ import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONObject;
 
@@ -26,9 +37,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-public class MyInterestsFragment extends Fragment {
+public class MyInterestsFragment extends Fragment implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
+    private GoogleApiClient mGoogleClient;
+    private LocationRequest mLocationRequest;
+    private Boolean mRequestLocationUpdates;
+    Location mLastLocation;
 
+    private Uri gmmIntentUri;
     private OnFragmentInteractionListener mListener;
     private Button button;
     private ProgressDialog progressDialog;
@@ -45,18 +64,57 @@ public class MyInterestsFragment extends Fragment {
         // Inflate the layout for this fragment
 
 
-
         View view = inflater.inflate(R.layout.fragment_my_interests, container, false);
+        if (mGoogleClient == null) {
+            mGoogleClient = new GoogleApiClient.Builder(getContext())
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+        mGoogleClient.connect();
+        createLocationRequest();
         button = (Button) view.findViewById(R.id.find_food);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Toast.makeText(getContext(),"button clicked", Toast.LENGTH_LONG).show();
-                volleyJsonObjectRequest("http://sample-env.vffm7ydwsk.us-west-2.elasticbeanstalk.com/api/suggestion?lat=28.0221939086914&long=-82.541389465332&history=false");
+
+                volleyJsonObjectRequest("http://sample-env.vffm7ydwsk.us-west-2.elasticbeanstalk.com/api/suggestion?lat=" + String.valueOf(mLastLocation.getLatitude())+ "&long=" + String.valueOf(mLastLocation.getLongitude()) + "&history=false");
+
             }
         });
         return view;
 
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mRequestLocationUpdates = true;
+        Log.d("MAP_API", "Location Request created yo!!!");
+    }
+
+    @Override
+    public void onStart() {
+        mGoogleClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        mGoogleClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleClient, this
+        );
     }
 
 
@@ -64,14 +122,14 @@ public class MyInterestsFragment extends Fragment {
         String REQUEST_TAG = "com.androidtutorialpoint.volleyJsonObjectRequest";
         progressDialog.setMessage("Loaging...");
         progressDialog.show();
-        StringRequest strReq = new StringRequest(url, new Response.Listener<String>(){
+        StringRequest strReq = new StringRequest(url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
 
             }
         }, new Response.ErrorListener() {
             @Override
-            public void onErrorResponse(VolleyError error){
+            public void onErrorResponse(VolleyError error) {
 
             }
         });
@@ -79,9 +137,9 @@ public class MyInterestsFragment extends Fragment {
         InterestVolleySingleton.getInstance(getContext()).addToRequestQueue(strReq, REQUEST_TAG);
     }
 
-    public void volleyJsonObjectRequest(String url){
+    public void volleyJsonObjectRequest(String url) {
 
-        String  REQUEST_TAG = "com.androidtutorialpoint.volleyJsonObjectRequest";
+        String REQUEST_TAG = "com.androidtutorialpoint.volleyJsonObjectRequest";
         progressDialog = new ProgressDialog(getContext());
         progressDialog.setMessage("Loading...");
         progressDialog.show();
@@ -107,6 +165,16 @@ public class MyInterestsFragment extends Fragment {
                         //outputTextView.setText(response.toString());
                         alertDialogBuilder.show();
                         progressDialog.hide();
+                        try {
+                            if(mLastLocation != null) {
+                                gmmIntentUri = Uri.parse("google.navigation:q=" + response.get("address").toString().replaceAll("\\s+", "+"));
+                                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                                mapIntent.setPackage("com.google.android.apps.maps");
+                                startActivity(mapIntent);
+                            }
+                        } catch(Exception e) {
+
+                        }
 
                     }
                 }, new Response.ErrorListener() {
@@ -115,18 +183,49 @@ public class MyInterestsFragment extends Fragment {
                 VolleyLog.d("Volley log", "Error: " + error.getMessage());
                 progressDialog.hide();
             }
-        })
-        {@Override
-        public Map<String, String> getHeaders(){
-            Map<String, String> parameters = new HashMap<>();
-            parameters.put("Authorization", "Bearer " + App.user_id_token);
-            return parameters;
-        }};
+        }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> parameters = new HashMap<>();
+                parameters.put("Authorization", "Bearer " + App.user_id_token);
+                return parameters;
+            }
+        };
 
         // Adding JsonObject request to request queue
-        InterestVolleySingleton.getInstance(getContext()).addToRequestQueue(jsonObjectReq,REQUEST_TAG);
+        InterestVolleySingleton.getInstance(getContext()).addToRequestQueue(jsonObjectReq, REQUEST_TAG);
     }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleClient);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
 
 
     /**
